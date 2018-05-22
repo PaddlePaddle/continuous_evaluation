98d29c4a23f04413e324e16f4b8499c3514f860d
Yan Chunwei <yanchunwei@outlook.com>
2018-05-22 13:00:06 +0800

    add pre-commit (#50)

---
+++ README.md
@@ -0,0 +1,5 @@
+# Paddle Continous Evaluate Framework
+## Howtos
+
+### Contribute
+- Run `pre-commit run -a` before your PR, this will help to format code automatically
--- web/main.py
+++ web/main.py
@@ -72,6 +72,13 @@ def commit_compare():
 
 
 if __name__ == '__main__':
-    host = '0.0.0.0'
-    port = 80
-    app.run(debug=False, host=host, port=port, threaded=True)
+    import argparse
+
+    parser = argparse.ArgumentParser(description='CE Web')
+    parser.add_argument('--port', type=int, default=80, required=False,
+                    help='web service port')
+
+    parser.add_argument('--host', type=str, default='0.0.0.0', required=False,
+                    help='web service host')
+    args = parser.parse_args()
+    app.run(debug=False, host=args.host, port=args.port, threaded=True)
