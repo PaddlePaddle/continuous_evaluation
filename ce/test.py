92ae32f3fd229b4100696803ad3516bd5b4e377b
superjomn <yanchunwei@outlook.com>
2018-07-17 19:33:23 +0800

    add test.py

---
+++ ce/test.py
@@ -0,0 +1,10 @@
+from ce.data_view_tester import *
+from ce.db_tester import *
+from ce.kpi_tester import *
+from ce.repo_tester import *
+from ce.utils_tester import *
+
+import unittest
+
+if __name__ == '__main__':
+    unittest.main()
