72cf2428c9a5aa957e306258414dc974702576c9
superjomn <yanchunwei@outlook.com>
2018-07-17 19:23:20 +0800

    clean

--- ce/main.py
+++ ce/main.py
@@ -134,7 +134,7 @@ def load_kpis(root, task_name):
         cmd = 'from %s.%s.continuous_evaluation import tracking_kpis' % (
             module, task_name)
         print('cmd', cmd)
-        exec(cmd, env)
+        exec (cmd, env)
         tracking_kpis = env['tracking_kpis']
         return tracking_kpis
 
