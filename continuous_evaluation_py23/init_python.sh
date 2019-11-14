c4b2047cc985b60f514eafb5de4e310830aa678b
kolinwei <331911734@qq.com>
2019-11-14 21:12:28 +0800

    Create init_python.sh

---
+++ continuous_evaluation_py23/init_python.sh
@@ -0,0 +1,5 @@
+export LD_LIBRARY_PATH=/opt/_internal/cpython-3.7.0/lib/:${LD_LIBRARY_PATH};
+export PATH=/opt/_internal/cpython-3.7.0/bin/:${PATH};
+export PYTHON_FLAGS='-DPYTHON_EXECUTABLE:FILEPATH=/opt/_internal/cpython-3.7.0/bin/python3.7
+            -DPYTHON_INCLUDE_DIR:PATH=/opt/_internal/cpython-3.7.0/include/python3.7m
+            -DPYTHON_LIBRARIES:FILEPATH=/opt/_internal/cpython-3.7.0/lib/libpython3.so';
